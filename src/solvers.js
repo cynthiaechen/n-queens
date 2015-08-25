d55ecf76f3478dcf78994b01bb3ba40af233a1a7
cynthiaechen <cynthiaechen@gmail.com>
2015-08-25 11:40:08 -0700

    solved count NRooks

--- src/solvers.js
+++ src/solvers.js
@@ -14,90 +14,92 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
-// ----------------------------------------------------------------------OLD CODE BELOW
-// window.findNRooksSolution = function(n, board) {
-//   var result = [];
-//   var solution = [];
-//   var board = board || new Board({n: n});
-
-//   var toggleCount = 0;
-//   var _helper = function(board, n){
-//     for(var j = 0; j < n; j++){ // j is row index
-//       for(var m = 0; m < n; m ++){ // m is column index
-//         if (board.get(j)[m] === 0) { //we are checking if there is a piece at (j,m) - if not, then add piece
-//           board.togglePiece(j, m); // we are adding a toggle piece 
-//           if(!board.hasAnyRooksConflicts()) { // if board has no conflicts
-//             toggleCount++;      //increase toggleCount by one
-//             if(toggleCount === n){          //and checking to see if there is a conflict. if there is none...
-//               var allRows = board.rows();
-//               for(var i = 0; i < n; i++) {
-//                 solution.push(allRows[i].slice());
-//               }
-//               return;
-//             }
-
-//             _helper(board, n);
-//           }
-//           board.togglePiece(j,m); //if there is a conflict, remove toggle
-//         }
-//       }
-//     }
-//   };
-
-//   _helper(board, n);
-
-//   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-//   // console.log(solution);
-//   return solution;
-
-// };
-
-
-
-// --------------------------------------------------------------------------
-
-
-window.findNRooksSolution = function(n, board) {
-  var result = [];
+window.findNRooksSolution = function(n) {
   var solution = [];
+  var board = board || new Board({n: n});
+  var allRows = board.rows();
+
+  var copy = function(matrix) {
+    var results = [];
+    for (var i = 0; i < matrix.length; i++) {
+      results.push(matrix[i].slice());
+    }
+    return results;
+
+  };
 
   var _helper = function(board, depth){
-    //depth at the very beginning is zero
-    //base case
-    //if(depth === n) { //if reaches bottom of matrix, then 
-        //if solution works (i.e. use helpers to validate), then copy solution and return out of function and go back to previous depth
-        //if not, then return out of function
-    //}
-    //loop through each row ---> specifically use depth variable to represent row index
-    //for (var i = 0; i < n; i++) { //looping through each element only in that row
-        //toggle on ith element in array (row)
-        //recurse on updated board => _helper(board, depth+1);
-        //toggle off ith element
-    //} 
+    
+    //BASE CASE
+    if(depth === n) {
+      if(!board.hasAnyRooksConflicts()) { //if there are no conflicts
+        solution.push(copy(allRows));
+      }
+      return;
+    }
 
-  //_helper(board, 0) depth is zero when we first invoke
+    //in this case for now, depth = 0
+
+    for(var i = 0; i < allRows[depth].length; i++){
+      board.togglePiece(depth, i);
+      if(!board.hasAnyRooksConflicts()){ //board has no conflict
+        _helper(board, depth + 1);
+      } 
+      board.togglePiece(depth, i);
+    }
+
+  };
+
+  _helper(board, 0);
+
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution[0]));
+  console.log(solution);
+  return solution[0];
 
 };
 
 
-
-
-
-
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
+  var solution = [];
+  var board = board || new Board({n: n});
+  var allRows = board.rows();
 
+  var copy = function(matrix) {
+    var results = [];
+    for (var i = 0; i < matrix.length; i++) {
+      results.push(matrix[i].slice());
+    }
+    return results;
 
-  // var solution = undefined; //fixme
-  var solutionCount = 0;
+  };
 
-  var arr = [];
+  var _helper = function(board, depth){
+    
+    //BASE CASE
+    if(depth === n) {
+      if(!board.hasAnyRooksConflicts()) { //if there are no conflicts
+        solution.push(copy(allRows));
+      }
+      return;
+    }
 
-  for(var i = 0; i < n; i++){
-     arr[i] = new Board({n: n}); // arr[i] is the board itself which is a n x n matrix
-     arr[i].togglePiece(0, i); // putting a toggle at a different column on top row of each board.
-     arr[i].findNRooksSolution(n, arr[i]);
-  }
+    //in this case for now, depth = 0
+
+    for(var i = 0; i < allRows[depth].length; i++){
+      board.togglePiece(depth, i);
+      if(!board.hasAnyRooksConflicts()){ //board has no conflict
+        _helper(board, depth + 1);
+      } 
+      board.togglePiece(depth, i);
+    }
+
+  };
+
+  _helper(board, 0);
+
+  var solutionCount = solution.length;
+
   
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
